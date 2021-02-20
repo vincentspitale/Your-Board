4a7502f4681fa4f67e3dd4a5999a8fd3b10089d8
Vincent Spitale <vincentgspitale@gmail.com>
2021-02-20 11:48:12 -0500

    new

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe src="https://www.figma.com/embed?embed_host=astra&url=https://www.figma.com/proto/qAoAPNoJwt6SZxAj4w2DOL/Untitled?node-id=1%3A2&scaling=min-zoom" frameborder="0">
+        </iframe>
+    </div>
+  );
+}
+
+export default App;

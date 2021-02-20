974999fd57b8cb99ddf634f717f77e2fc0b0ba1a
Kevin <kevinsebcam@gmail.com>
2021-02-20 16:56:40 -0500

    Import App.css

--- src/SideBar.js
+++ src/SideBar.js
@@ -8,11 +8,9 @@ const SideBar = ({figmaUrl, setFigmaUrl, ...props}) => {
   return (
     <div tw="w-400px pl-4 pr-4 pt-20">
       <div tw="pb-10">
-      <Time />
+        <Time />
       </div>
       <p tw="text-2xl font-semibold">Your Board</p>
-      <div>
-      </div>
     </div>
   );
 };
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,20 +1,22 @@
 import React from "react";
 import tw from 'twin.macro'
 
+import './App.css'
+
 function Welcome() {
   return(
-    <div tw="w-screen">
+    <div tw="w-screen" className="welcomeColor">
  
         <h1 className="first">Welcome to your Board</h1>
         <br></br>
       
       <ul className="list">
      
-      <span><p className="one">1</p></span><li className="second">Create a new Figma project</li>
-      <span><p className="two">2</p></span><li className="third">Share a link with edit access</li>
-      <span><p className="three">3</p></span><li className="fourth">Send it to your friends</li>
-      <span><p className="four">4</p></span><li className="fifth">Paste the link here</li>
-      <span><p className="five">5</p></span><li className="sixth">See the changes live</li>
+      <span><p className="one">1</p><li className="second">Create a new Figma project</li></span>
+      <span><p className="two">2</p><li className="third">Share a link with edit access</li></span>
+      <span><p className="three">3</p><li className="fourth">Send it to your friends</li></span>
+      <span><p className="four">4</p><li className="fifth">Paste the link here</li></span>
+      <span><p className="five">5</p><li className="sixth">See the changes live</li></span>
       </ul>
       
     </div>
@@ -27,6 +29,6 @@ function Welcome() {
       
     </div>
   );
-};
+};*/
 
 export default Welcome;

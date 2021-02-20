922842d0d177b751e2e751b07265fe1e1f91fcb7
Chris Wu <christopherjwu2000@gmail.com>
2021-02-20 17:06:30 -0500

    my fix

--- src/Welcome.js
+++ src/Welcome.js
@@ -9,8 +9,8 @@ function Welcome() {
       
       <ul className="list">
      
-      <span><p className="one">1</p></span><li className="second">Create a new Figma project</li>
-      <span><p className="two">2</p></span><li className="third">Share a link with edit access</li>
+      <span><p className="one">1</p><li className="second">Create a new Figma project</li></span>
+      <span><p className="two">2</p><li className="third">Share a link with edit access</li></span>
       <span><p className="three">3</p></span><li className="fourth">Send it to your friends</li>
       <span><p className="four">4</p></span><li className="fifth">Paste the link here</li>
       <span><p className="five">5</p></span><li className="sixth">See the changes live</li>

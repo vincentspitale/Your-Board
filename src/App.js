10f681b75450e21bc9c0af4dad9a291e4e000a54
Vincent Spitale <vincentgspitale@gmail.com>
2021-02-20 12:55:27 -0500

    Revert "Revert "Set up twin macro.""
    This reverts commit 06a7b5dffe74361dbdf0c33a982f6a49efcad36b.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import tw from 'twin.macro'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" tw="bg-red-400">
       <iframe src="https://www.figma.com/embed?embed_host=astra&url=https://www.figma.com/proto/qAoAPNoJwt6SZxAj4w2DOL/Untitled?node-id=1%3A2&scaling=min-zoom" frameborder="0">
         </iframe>
+      <p>Hello World</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import GlobalStyles from './GlobalStyles'
+
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root')

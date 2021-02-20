3381e36b8ad0683f829071b9a713edfe44139bfa
Vincent Spitale <vincentgspitale@gmail.com>
2021-02-20 01:02:04 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Toggle from './components/toggle/toggle';
 
 
 function App() {
   return (
     <div className="App">
-    <h1 className="holo">Penumbra</h1>
-    <Toggle />
     </div>
   );
 }
